eb8b48cc8a164e9b2e3c8f9e41c5aacb8bebefc7
Maciej Janicki <maciej.ja@proton.me>
2024-07-13 14:51:35 +0200

    Feature: interpolation

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
+#include <assert.h>
+#include <stdlib.h>
+
 #include "raylib.h"
-#include "raymath.h"
-const int cw = 800;
-const int ch = 450;
+#include "structs.h"
+const int cw = 1280;
+const int ch = 720;
 
 
 void put_pixel(int x, int y, Color c);
 
-void draw_line(Vector2 pa, Vector2 pb, Color c);
+void draw_line(iVector2 pa, iVector2 pb, Color c);
+
+void interpolate(int i0, int d0, int i1, int d1, int *output);
 
 int main(void) {
   InitWindow(cw, ch, "rasterizer");
-  Vector2 pa = {0, 0};
-  Vector2 pb = {200, 200};
+  iVector2 pa = {0, 0};
+  iVector2 pb = {2000, -100};
 
   while (!WindowShouldClose()) {
     BeginDrawing();
@@ -26,16 +31,49 @@ int main(void) {
 }
 
 void put_pixel(int x, int y, Color c) {
-  Vector2 pos = (Vector2){.x = x + cw / 2, .y = -y + ch / 2};
-  DrawPixelV(pos, c);
+  iVector2 pos = {.x = x + cw / 2, .y = -y + ch / 2};
+  DrawPixel(pos.x, pos.y, c);
 }
 
-void draw_line(Vector2 pa, Vector2 pb, Color c) {
-  float alpha = (float) (pb.y - pa.y) / (float) (pb.x - pa.x);
-  int b = pa.y - alpha * pa.x;
-  for (int x = pa.x; x <= pb.x; ++x) {
-    int y;
-    y = alpha * x + b;
-    put_pixel(x, y,BLACK);
+void interpolate(int i0, int d0, int i1, int d1, int *output) {
+  assert(output!=NULL && "Null pointer passed to interpolation");
+  if (i0 == i1) {
+    output[0] = d0;
+  }
+  const float a = (float) (d1 - d0) / (float) (i1 - i0);
+  float d = d0;
+  for (int i = i0; i <= i1; ++i) {
+    output[i - i0] = d;
+    d += a;
+  }
+}
+
+void draw_line(iVector2 pa, iVector2 pb, Color c) {
+  int dx = abs(pb.x - pa.x);
+  int dy = abs(pb.y - pa.y);
+  if (dx > dy) {
+    if (pb.x < pa.x) {
+      iVector2 tmp = pb;
+      pb = pa;
+      pa = tmp;
+    }
+    int *y = malloc(sizeof(int) * (dx + 1));
+    interpolate(pa.x, pa.y, pb.x, pb.y, y);
+    for (int x = pa.x; x <= pb.x; ++x) {
+      put_pixel(x, y[x - pa.x], c);
+    }
+    free(y);
+  } else {
+    if (pb.y < pa.y) {
+      iVector2 tmp = pb;
+      pb = pa;
+      pa = tmp;
+    }
+    int *x = malloc(sizeof(int) * (dy + 1));
+    interpolate(pa.y, pa.x, pb.y, pb.x, x);
+    for (int y = pa.y; y <= pb.y; ++y) {
+      put_pixel(x[y - pa.y],y, c);
+    }
+    free(x);
   }
 }
---
+++ structs.h
@@ -0,0 +1,12 @@
+//
+// Created by maciej on 13/07/24.
+//
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
+typedef struct{
+  int x;
+  int y;
+} iVector2;
+
+#endif //STRUCTS_H
