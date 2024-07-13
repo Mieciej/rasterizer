8817a8e1d26018ffccb0064d9fedc37d805f7e65
Maciej Janicki <maciej.ja@proton.me>
2024-07-13 14:05:21 +0200

    Refactor: Line Drawing

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ const int ch = 450;
 
 void put_pixel(int x, int y, Color c);
 
+void draw_line(Vector2 pa, Vector2 pb, Color c);
+
 int main(void) {
   InitWindow(cw, ch, "rasterizer");
   Vector2 pa = {0, 0};
@@ -13,14 +15,8 @@ int main(void) {
 
   while (!WindowShouldClose()) {
     BeginDrawing();
+    draw_line(pa, pb, BLACK);
     ClearBackground(RAYWHITE);
-    for (int x = pa.x; x <= pb.x; ++x) {
-      int y;
-      float alpha = (float) (pb.y - pa.y) / (float) (pb.x - pa.x);
-      int b = pa.y - alpha * pa.x;
-      y = alpha * x + b;
-      put_pixel(x, y,BLACK);
-    }
     EndDrawing();
   }
 
@@ -32,4 +28,14 @@ int main(void) {
 void put_pixel(int x, int y, Color c) {
   Vector2 pos = (Vector2){.x = x + cw / 2, .y = -y + ch / 2};
   DrawPixelV(pos, c);
+}
+
+void draw_line(Vector2 pa, Vector2 pb, Color c) {
+  float alpha = (float) (pb.y - pa.y) / (float) (pb.x - pa.x);
+  int b = pa.y - alpha * pa.x;
+  for (int x = pa.x; x <= pb.x; ++x) {
+    int y;
+    y = alpha * x + b;
+    put_pixel(x, y,BLACK);
+  }
 }
